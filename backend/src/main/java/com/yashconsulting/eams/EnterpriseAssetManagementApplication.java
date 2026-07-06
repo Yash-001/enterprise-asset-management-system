df933cfcf1f032baa023a634a29df9472a22a783
yash <yashranjan@eworldes.com>
2026-07-06 07:27:04 +0530

    Setup Flyway database migration.

--- backend/src/main/java/com/yashconsulting/eams/EnterpriseAssetManagementApplication.java
+++ backend/src/main/java/com/yashconsulting/eams/EnterpriseAssetManagementApplication.java
@@ -3,10 +3,15 @@ package com.yashconsulting.eams;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class EnterpriseAssetManagementApplication {
 
 	public static void main(String[] args) {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
 		SpringApplication.run(EnterpriseAssetManagementApplication.class, args);
 	}
 
--- backend/src/test/java/com/yashconsulting/eams/EnterpriseAssetManagementApplicationTests.java
+++ backend/src/test/java/com/yashconsulting/eams/EnterpriseAssetManagementApplicationTests.java
@@ -2,12 +2,15 @@ package com.yashconsulting.eams;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.jpa.properties.hibernate.jdbc.time_zone=UTC"
+})
 class EnterpriseAssetManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
