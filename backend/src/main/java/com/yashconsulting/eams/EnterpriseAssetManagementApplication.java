e6e90182fe72d7b72e8a296ba7f7134578409923
yash <yashranjan@eworldes.com>
2026-07-06 06:26:23 +0530

    Setup PostgreSQL with Docker.

---
+++ backend/src/main/java/com/yashconsulting/eams/EnterpriseAssetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.yashconsulting.eams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAssetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseAssetManagementApplication.class, args);
+	}
+
+}
