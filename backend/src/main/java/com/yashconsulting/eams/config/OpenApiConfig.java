4e62ad371ba5ce613cb4e45cbeacb26894e9e40d
yash <yashranjan@eworldes.com>
2026-07-08 17:50:37 +0530

    feat(swagger): add JWT bearer authentication support.

--- backend/src/main/java/com/yashconsulting/eams/config/OpenApiConfig.java
+++ backend/src/main/java/com/yashconsulting/eams/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.yashconsulting.eams.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +15,16 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI enterpriseAssetManagementOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
                 .info(new Info()
                         .title("Enterprise Asset Management API")
                         .version("v1.0")
